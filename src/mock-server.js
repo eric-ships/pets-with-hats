321f9de72f301f09fc021ce9b0e665d4ab5aecc3
Eric Liu <ericliu121187@gmail.com>
2017-08-05 16:42:10 -0400

    make sure timestamp of response is after parent's; create nested responses

--- src/mock-server.js
+++ src/mock-server.js
@@ -4,23 +4,31 @@ const messages = []
 
 function generateMessages() {
   const originalMessageCount = faker.random.number({ min: 1, max: 200 }) - 1
+  const timestamps = {}
   const totalMessageCount =
     faker.random.number({ min: originalMessageCount, max: 1000 }) - 1
 
   for (let i = 0; i <= originalMessageCount; i++) {
+    const timestamp = Date.parse(faker.date.past())
+
     messages.push({
       id: i,
       message: faker.hacker.phrase(),
-      timestamp: Date.parse(faker.date.past()),
+      timestamp,
     })
+
+    timestamps[i] = timestamp
   }
 
   for (let i = originalMessageCount; i <= totalMessageCount; i++) {
+    const parent = faker.random.number(totalMessageCount - 1)
+    const timestamp = Date.parse(faker.date.future(1, Date(timestamps[parent])))
+
     messages.push({
       id: i,
       message: faker.hacker.phrase(),
-      parent: faker.random.number(originalMessageCount - 1),
-      timestamp: Date.parse(faker.date.past()),
+      parent,
+      timestamp,
     })
   }
 }
