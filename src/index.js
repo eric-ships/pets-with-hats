0c12352dd6ce61672834b9b381ec0c4f911c0e29
Eric Liu <ericliu121187@gmail.com>
2017-08-06 09:07:34 -0400

    remove index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import App from './components/app'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import 'bulma/css/bulma.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
