ea0a6ab9f29f145b04a01a58e236daf5c8236199
Eric Liu <ericliu121187@gmail.com>
2017-08-05 20:45:07 -0400

    add reply button

--- src/components/container/messages.js
+++ src/components/container/messages.js
@@ -23,11 +23,33 @@ const defaultProps = {
 class Messages extends Component {
   constructor(props) {
     super(props)
+    this.focusReplyInput = this.focusReplyInput.bind(this)
     this.toggleVisibility = this.toggleVisibility.bind(this)
 
+    this.replyInput = null
+
     this.state = {
+      isReplying: false,
       isVisible: props.areOriginalMessageIds,
     }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.isReplying && this.state.isReplying) {
+      this.replyInput.focus()
+    }
+  }
+
+  focusReplyInput() {
+    if (this.state.isVisible) {
+      this.replyInput.focus()
+      return
+    }
+
+    this.setState({
+      isReplying: true,
+      isVisible: true,
+    })
   }
 
   toggleVisibility() {
@@ -38,23 +60,43 @@ class Messages extends Component {
 
   render() {
     const { areOriginalMessageIds, ids, messagesById } = this.props
-    const { isVisible } = this.state
+    const { isReplying, isVisible } = this.state
     const isEmpty = ids.length === 0
+    const isReplyable = isVisible || isReplying
 
     return (
       <div className="messages">
         {!areOriginalMessageIds &&
-          <button
-            className={`button is-outlined is-small ${isEmpty
-              ? ''
-              : 'is-info'}`}
-            disabled={isEmpty}
-            onClick={this.toggleVisibility}
-          >
-            {`${isVisible ? 'hide' : 'show'} responses (${ids.length})`}
-          </button>}
+          <div className="is-clearfix">
+            <button
+              className="button is-outlined is-small is-pulled-right is-primary"
+              onClick={this.focusReplyInput}
+            >
+              {'Reply'}
+            </button>
+
+            <button
+              className={`button is-outlined is-small is-pulled-right ${isEmpty
+                ? ''
+                : 'is-info'}`}
+              disabled={isEmpty}
+              onClick={this.toggleVisibility}
+              style={{ marginRight: '0.25rem' }}
+            >
+              {`${isVisible ? 'Hide' : 'Show'} responses (${ids.length})`}
+            </button>
+          </div>}
 
         {isVisible && <MessageList ids={ids} messagesById={messagesById} />}
+
+        {isReplyable &&
+          <input
+            className="input is-small"
+            placeholder="Reply..."
+            ref={n => (this.replyInput = n)}
+            style={{ marginTop: '1rem' }}
+            type="text"
+          />}
       </div>
     )
   }
